9f52096df7f91ec2dbd96d8551ce8a4b485a86f1
kj27july <79355765+kj27july@users.noreply.github.com>
2021-03-08 12:34:50 +0530

    Add files via upload

---
+++ DeviceModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1_os.Models
+{
+    public class DeviceModel
+    {
+        public int id { get; set; }
+        public String Name { get; set; }
+        public String OsType { get; set; }
+        public int Battery { get; set; }
+        public int AvailableMemory { get; set; }
+    }
+}
